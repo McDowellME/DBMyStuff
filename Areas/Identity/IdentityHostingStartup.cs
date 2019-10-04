3d1af37a2551e7e315f0abb21fd1ef6fdbee19a7
Melinda McDowell <melinda.e.mcdowell@outlook.com>
2019-10-04 14:32:42 -0700

    Added working authentication and migration to DB

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PersonalPropertyApp.Models;
+
+[assembly: HostingStartup(typeof(PersonalPropertyApp.Areas.Identity.IdentityHostingStartup))]
+namespace PersonalPropertyApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<PersonalPropertyAppContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("PersonalPropertyAppContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<PersonalPropertyAppContext>();
+            });
+        }
+    }
+}
